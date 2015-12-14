16315c9c5a6d3e9f256ab988b35fccd946439e29
Daham Drake Wang <drakewang@gmail.com>
2015-12-14 06:19:03 -0800

    implemented 10 min if

--- tips/SettingsViewController.swift
+++ tips/SettingsViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    // initialize memory
     let defaults = NSUserDefaults.standardUserDefaults()
     
+    // initialize this panel
     @IBOutlet weak var tipControl_2: UISegmentedControl!
     
     override func viewDidLoad() {
@@ -19,17 +21,24 @@ class SettingsViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    // store the date to memory on closing this page
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        defaults.setDouble(NSDate().timeIntervalSince1970, forKey: "myDate")
+    }
+    
+    // keep the field equal to whatever it was set
+    // in TipViewController
     override func viewWillAppear(animated: Bool) {
         super.viewDidAppear(animated)
         tipControl_2.selectedSegmentIndex = defaults.integerForKey("myNum")
     }
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
@@ -41,6 +50,8 @@ class SettingsViewController: UIViewController {
     }
     */
 
+    // store tipControl in memory (to be re-used in the
+    // ViewController page)
     @IBAction func onEditingChange_2(sender: AnyObject) {
         defaults.setInteger(tipControl_2.selectedSegmentIndex, forKey: "myNum")
     }
--- tips/TipViewController.swift
+++ tips/TipViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TipViewController.swift
 //  tips
 //
 //  Created by Daham Wang on 12/13/15.
@@ -10,13 +10,16 @@ import UIKit
 
 class TipViewController: UIViewController {
     
+    // initialize memory
     let defaults = NSUserDefaults.standardUserDefaults()
 
+    // initialize panels and fields
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    // tip and total are initialized to 0 onload
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,12 +27,26 @@ class TipViewController: UIViewController {
         totalLabel.text = "$0.00"
     }
     
+    // if time elapsed is less than 10 minutes, 
+    //   set tipControl and billField equal to memory
+    //   call onEditingChange()
     override func viewWillAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        tipControl.selectedSegmentIndex = defaults.integerForKey("myNum")
-        billField.text = defaults.stringForKey("myField")
         
-        onEditingChange(0)
+        let prevTime = defaults.doubleForKey("myDate")
+        let currTime = NSDate().timeIntervalSince1970
+
+        if currTime - prevTime < 600 {
+            tipControl.selectedSegmentIndex = defaults.integerForKey("myNum")
+            billField.text = defaults.stringForKey("myField")
+            onEditingChange(0)
+        }
+    }
+    
+    // store the date to memory on closing this page
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        defaults.setDouble(NSDate().timeIntervalSince1970, forKey: "myDate")
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,6 +54,8 @@ class TipViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // update fields based on tipControl and billField
+    // store tipControl and billField to memory
     @IBAction func onEditingChange(sender: AnyObject) {
         let tipPercentages = [0.18, 0.2, 0.22]
         let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
